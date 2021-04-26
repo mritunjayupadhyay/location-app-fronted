717530ce989d597a4f4e21f62718cc8dfb67ba17
Mritunjay Upadhyay <mupadhyay00@gmail.com>
2021-04-26 17:11:40 +0700

    feat: Add header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { AppComponent } from './../app.component';
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: [
+    './header.component.scss'
+  ]
+})
+export class HeaderComponent {}
