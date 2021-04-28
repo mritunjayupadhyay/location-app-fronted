06fee36bf9b61377fa4e749a0d12e7afad9d4a82
Mritunjay Upadhyay <mupadhyay00@gmail.com>
2021-04-29 00:52:35 +0700

    feat: Make login form

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
+  activeInput = "input_form_sign d_block active_inp";
+  inActiveInput = "input_form_sign";
+  sign_in_class = "active";
+  sign_up_class = "inActive"
+  inputClass = "input_form_sign";
+  isSignIn = true;
 
+  name_us = '';
+  email_us = '';
+  pass_us = '';
+  conf_pass_us = '';
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  sign_in() {
+    this.sign_in_class = "active";
+    this.sign_up_class = "inActive";
+    this.inputClass = this.inActiveInput;
+    this.isSignIn = true;
+  }
+
+  sign_up() {
+    this.sign_in_class = "inActive";
+    this.sign_up_class = "active";
+    this.inputClass = this.activeInput;
+    this.isSignIn = false;
+  }
+
+  onChangeText(event) {
+    if (!event) { return; }
+    const { name, value  } = event.target;
+    this[name] = value;
   }
 
 }
