e2a59530042ce3c89c71c116735a5f493865fd14
Mritunjay Upadhyay <mupadhyay00@gmail.com>
2021-05-13 03:46:42 +0700

    feat: Add dockerfile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: "http://localhost:8000",
+  baseUrl: "http://54.254.193.80:8000",
   googleApiKey: "AIzaSyCuZW2gH7j7IlW_PLKSsSFEtQqrznYCXh4"
 };
